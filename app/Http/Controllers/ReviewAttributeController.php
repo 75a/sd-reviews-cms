2ed394a8a3c1e919cff78e0a6fa43e9f5e53ae9a
Michal Brzozowski <contact@michalbrzozowski.com>
2021-01-23 01:45:32 +0100

    Bulk create initial controllers for every identified model

---
+++ app/Http/Controllers/ReviewAttributeController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ReviewAttribute;
+use Illuminate\Http\Request;
+
+class ReviewAttributeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ReviewAttribute  $reviewAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ReviewAttribute $reviewAttribute)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ReviewAttribute  $reviewAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ReviewAttribute $reviewAttribute)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ReviewAttribute  $reviewAttribute
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ReviewAttribute $reviewAttribute)
+    {
+        //
+    }
+}
