6d942b5a12614712801b734632cc237a8ec35eff
Michal Brzozowski <contact@michalbrzozowski.com>
2021-01-24 19:50:29 +0100

    Implement initial CRUD methods in MenuLinkController

--- app/Http/Controllers/MenuLinkController.php
+++ app/Http/Controllers/MenuLinkController.php
@@ -2,41 +2,68 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\MenuLinkResource;
 use App\Models\MenuLink;
+use http\Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use function MongoDB\BSON\toJSON;
 
 class MenuLinkController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\Resources\Json\AnonymousResourceCollection
      */
     public function index()
     {
-        //
+        return MenuLinkResource::collection(MenuLink::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Http\JsonResponse
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'menu_name' => ['required'],
+            'label' => ['required'],
+            'url' => ['required'],
+            'zindex' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toArray(), 422);
+        }
+
+        DB::beginTransaction();
+        try {
+            $menuLink = new MenuLink();
+            $menuLink->fill($request->all());
+            $menuLink->save();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json($e->getMessage(), 409);
+        }
+
+        return (new MenuLinkResource($menuLink))->response();
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\MenuLink  $menuLink
-     * @return \Illuminate\Http\Response
+     * @return MenuLinkResource
      */
     public function show(MenuLink $menuLink)
     {
-        //
+        return new MenuLinkResource($menuLink);
     }
 
     /**
@@ -44,21 +71,28 @@ class MenuLinkController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\MenuLink  $menuLink
-     * @return \Illuminate\Http\Response
+     * @return MenuLinkResource
      */
     public function update(Request $request, MenuLink $menuLink)
     {
-        //
+        $menuLink->update($request->all());
+        return new MenuLinkResource($menuLink);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\MenuLink  $menuLink
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param \App\Models\MenuLink $menuLink
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(MenuLink $menuLink)
+    public function destroy(Request $request, MenuLink $menuLink)
     {
-        //
+        try {
+            $menuLink->delete();
+        } catch (\Exception $e) {
+            abort(400, $e->getMessage());
+        }
+        return response()->json(null, 204);
     }
 }
--- app/Models/MenuLink.php
+++ app/Models/MenuLink.php
@@ -9,4 +9,16 @@ class MenuLink extends Model
 {
     use HasFactory;
     protected $table = "menu_links";
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'menu_name',
+        'label',
+        'url',
+        'zindex'
+    ];
 }
