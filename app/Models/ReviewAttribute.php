80fc206e86f4d02243f84be35eca19694504b457
Michal Brzozowski <contact@michalbrzozowski.com>
2021-01-24 20:26:40 +0100

    Add fillable array to ReviewAttribute model

--- app/Models/ReviewAttribute.php
+++ app/Models/ReviewAttribute.php
@@ -10,6 +10,16 @@ class ReviewAttribute extends Model
     use HasFactory;
     protected $table = "review_attributes";
 
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'label',
+        'is_nullable'
+    ];
+
     public function reviews()
     {
         return $this->belongsToMany(Review::class);
