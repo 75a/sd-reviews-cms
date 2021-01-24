a9ac00ba2f498fb04e6f7f8af57b386441f30945
Michal Brzozowski <contact@michalbrzozowski.com>
2021-01-24 20:48:00 +0100

    Implement initial CRUD methods in CommentController

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,65 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use App\Models\Review;
+use http\Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @param Review $review
-     * @return \Illuminate\Http\JsonResponse
+     * @return AnonymousResourceCollection
      */
-    public function index(Review $review)
+    public function index()
     {
-        return response()->json($review->comments);
+        return CommentResource::collection(Comment::all());
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Review $review
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function store(Request $request, Review $review)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'user_id' => ['required', 'exists:users,id'],
+            'content' => ['required']
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors()->toArray(), 422);
+        }
+
+        DB::beginTransaction();
+        try {
+            $comment = new Comment();
+            $comment->fill($request->all());
+            $comment->review_id = $review->id;
+            $comment->save();
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+            return response()->json($e->getMessage(), 409);
+        }
+
+        return (new CommentResource($comment))->response();
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
+     * @param Review $review
+     * @param Comment $comment
+     * @return CommentResource
      */
-    public function show(Comment $comment)
+    public function show(Review $review, Comment $comment)
     {
-        //
+        return new CommentResource($comment);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @param Review $review
+     * @param Comment $comment
+     * @return CommentResource
      */
-    public function update(Request $request, Comment $comment)
+    public function update(Request $request, Review $review, Comment $comment)
     {
-        //
+        $comment->update($request->all());
+        return new CommentResource($comment);
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Comment  $comment
-     * @return \Illuminate\Http\Response
+     * @param Review $review
+     * @param Comment $comment
+     * @return JsonResponse
      */
-    public function destroy(Comment $comment)
+    public function destroy(Review $review, Comment $comment)
     {
-        //
+        try {
+            $comment->delete();
+        } catch (\Exception $e) {
+            abort(400, $e->getMessage());
+        }
+        return response()->json(null, 204);
     }
 }
