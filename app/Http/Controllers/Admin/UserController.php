79ee9181ae5e334c137decbe47a44ff1165b7f6e
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-07 15:49:03 +0100

    Add response wrapping

--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -7,57 +7,53 @@ use App\Http\Requests\UserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\Role;
 use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use Symfony\Component\HttpKernel\Exception\HttpException;
+use Throwable;
 
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(): JsonResponse
     {
-        return UserResource::collection(User::all());
+        return response()->json(UserResource::collection(User::all()));
     }
 
-    public function store(UserRequest $request)
+    public function store(UserRequest $request): JsonResponse
     {
         try {
-
             $user = new User;
             $user->fill($request->validated());
             $user->password = Hash::make($user->password);
             $user->role()
                 ->associate(Role::where('name',env('ROLE_NAME_UNVERIFIED'))
                 ->first());
-
             if ($user->save()){
-                return new UserResource($user);
+                return response()->json(new UserResource($user));
             }
-        } catch(\Exception $exception) {
-
+        } catch(Throwable $exception) {
             abort(400, $exception->getMessage());
-
         }
-        return null;
+        return response()->json(null,400);
     }
 
-    public function show(User $user)
+    public function show(User $user): JsonResponse
     {
-        //return response()->json($user);
-        return new UserResource($user);
+        return response()->json(new UserResource($user));
     }
 
-    public function update(Request $request, User $user)
+    public function update(Request $request, User $user): JsonResponse
     {
         $user->update($request->all());
-        return new UserResource($user);
+        return response()->json(new UserResource($user));
     }
 
-    public function destroy(User $user)
+    public function destroy(User $user): JsonResponse
     {
         try {
             $user->delete();
-        } catch (\Exception $e) {
+        } catch (Throwable $e) {
             abort(400, $e->getMessage());
         }
         return response()->json(['message' => 'User deleted'], 204);
