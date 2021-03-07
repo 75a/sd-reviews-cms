da90350822c105b0eb8d0c6a4ad247e885384d9b
Michal Brzozowski <contact@michalbrzozowski.com>
2021-03-07 15:14:03 +0100

    Create comment controller

--- app/Http/Controllers/Admin/CommentController.php
+++ app/Http/Controllers/Admin/CommentController.php
@@ -6,33 +6,20 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\CommentResource;
 use App\Models\Comment;
 use App\Models\Review;
-use http\Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
+use Throwable;
 
 class CommentController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return AnonymousResourceCollection
-     */
-    public function index()
+    public function index(Review $review): JsonResponse
     {
-        return CommentResource::collection(Comment::all());
+        return response()->json(CommentResource::collection($review->comments));
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param Request $request
-     * @param Review $review
-     * @return JsonResponse
-     */
-    public function store(Request $request, Review $review)
+    public function store(Request $request, Review $review): JsonResponse
     {
         $validator = Validator::make($request->all(), [
             'user_id' => ['required', 'exists:users,id'],
@@ -47,55 +34,33 @@ class CommentController extends Controller
         try {
             $comment = new Comment();
             $comment->fill($request->all());
-            $comment->review_id = $review->id;
+            $review->comments()->save($comment);
             $comment->save();
             DB::commit();
-        } catch (Exception $e) {
+        } catch (Throwable $e) {
             DB::rollBack();
             return response()->json($e->getMessage(), 409);
         }
 
-        return (new CommentResource($comment))->response();
+        return response()->json(new CommentResource($comment), 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param Review $review
-     * @param Comment $comment
-     * @return CommentResource
-     */
-    public function show(Review $review, Comment $comment)
+    public function show(Review $review, Comment $comment): JsonResponse
     {
-        return new CommentResource($comment);
+        return response()->json(new CommentResource($comment));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param Request $request
-     * @param Review $review
-     * @param Comment $comment
-     * @return CommentResource
-     */
-    public function update(Request $request, Review $review, Comment $comment)
+    public function update(Request $request, Review $review, Comment $comment): JsonResponse
     {
         $comment->update($request->all());
-        return new CommentResource($comment);
+        return response()->json(new CommentResource($comment));
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param Review $review
-     * @param Comment $comment
-     * @return JsonResponse
-     */
-    public function destroy(Review $review, Comment $comment)
+    public function destroy(Review $review, Comment $comment): JsonResponse
     {
         try {
             $comment->delete();
-        } catch (\Exception $e) {
+        } catch (Throwable $e) {
             abort(400, $e->getMessage());
         }
         return response()->json(null, 204);
